6722129dee6938706790b880e7147e62bafb133c
kamilpieczyk <kamil.pieczyk@gmail.com>
2022-02-18 15:39:36 +0000

    ADD PROMETHEUS METRICS ROUTE   -add prometheus middleware,   -add /metrics route,

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,28 @@
 import { Application } from "express";
+import promMid from "express-prometheus-middleware";
 
 import GET, { GETRoute } from "./routes/GET";
 
 class App {
   constructor(app: Application, port: number) {
+    // use prometheus middleware
+    app.use(
+      promMid({
+        metricsPath: "/metrics",
+        collectDefaultMetrics: true,
+        collectGCMetrics: true,
+      })
+    );
+
     this.get(app);
+
     app.listen(port, () => {
       console.log(`🚀 app is running at port: ⚓${port}`);
     });
   }
 
   get(app: Application) {
+    // this method handles all GET requests
     GET.forEach((getRoute: GETRoute) => {
       app.get(getRoute.route, getRoute.func);
     });
